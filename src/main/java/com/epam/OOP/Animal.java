97f7cc4a37a8825be3483f15cdf63378a0b7f629
Sergey Tsynin <Sergey.Tsynin@gmail.com>
2022-12-23 23:33:47 +0100

    dog and bird classes implemented (OOP Task2)

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,6 +1,16 @@
 package com.epam.OOP;
 
 public class Animal {
+
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        System.out.println(dog.getDescription());
+        Animal bird = new Bird();
+        System.out.println(bird.getDescription());
+        Animal fly = new Animal("black", 6, false);
+        System.out.println(fly.getDescription());
+    }
+
     private String color;
     private int numberOfPaws;
     private boolean hasFur;
