e9b2b710a75fd5c199e8c92083dfb7a6796279d4
Sergey Tsynin <Sergey.Tsynin@gmail.com>
2022-12-24 17:27:07 +0100

    Days in a month implemented (Conditions Task3)

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,16 @@
 package com.epam.conditions;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+        try {
+            LocalDate date = LocalDate.of(year, month, 1);
+            System.out.println(date.lengthOfMonth());
+        } catch (DateTimeException e) {
+            System.out.println("invalid date");
+        }
     }
-
 }
