5ce59cdf01c7b384394480b4a9bee5ddbff1ecfe
Sergey Tsynin <Sergey.Tsynin@gmail.com>
2022-12-24 17:59:15 +0100

    Coordinate Pane implemented (Conditions Task5)

--- src/main/java/com/epam/conditions/CoordinatePane.java
+++ src/main/java/com/epam/conditions/CoordinatePane.java
@@ -3,7 +3,12 @@ package com.epam.conditions;
 public class CoordinatePane {
 
     public void printQuadrant(int x, int y) {
+        if (x == 0 && y == 0) {
+            System.out.println("zero");
+            return;
+        }
+        String[][] pane = {{"third", "second"}, {"fourth", "first"}};
+        System.out.println(pane[x > 0 ? 1 : 0][y > 0 ? 1 : 0]);
     }
-
 }
 
